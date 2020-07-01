d4bcfc8866e1fe820b4d44776f1722803d5093e8
2019315007_LeeJooYeol <gametyr@naver.com>
2020-07-01 13:58:45 +0900

    [message-01] Add the message list

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,14 +1,31 @@
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
-
+# list of messages
+messages = ["Tie!", "You win!", "You lose!"]
 # assign a random play to the computer
 computer = "Rock"
 print('Computer: {}'.format(computer))
-
 # get the user input
-player = "Paper"
+player = input("Rock, Paper, Scissors? ")
 print('Player: {}'.format(player))
-
 # tie
 if player == computer:
-     print("Tie!")
+     print(messages[0])
+# rock
+elif player == "Rock":
+    if computer == "Scissors":
+         print(messages[1])
+    else:
+         print(messages[2])
+# paper
+elif player == "Paper":
+    if computer == "Rock":
+         print(messages[1])
+    else:
+         print(messages[2])
+# scissors
+elif player == "Scissors":
+    if computer == "Paper":
+         print(messages[1])
+    else:
+         print(messages[2])
